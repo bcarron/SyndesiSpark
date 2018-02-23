c437424171df7681b324fd791099f1c7090075b8
Blaise <blaise.carron@etu.unige.ch>
2018-02-23 16:17:09 +0100

    Change Spark master

--- src/main/java/Syndesi.java
+++ src/main/java/Syndesi.java
@@ -4,14 +4,15 @@ import org.apache.spark.sql.types.Metadata;
 import org.apache.spark.sql.types.StructField;
 import org.apache.spark.sql.types.StructType;
 
-public class Syndesi {
+public class Syndesi { 
     public static void main(String[] args) {
         SparkSession spark = SparkSession
                 .builder()
                 .appName("Syndesi Spark")
-                .config("spark.master", "local") //spark://ubuntu:7077
+                .config("spark.master", "spark://129.194.246.176:7077") //spark://ubuntu:7077 spark://129.194.246.176:7077
                 .getOrCreate();
 
+        // Actual DB connection
 /*        Dataset<Row> data = spark.read().format("jdbc")
                 .option("url", "jdbc:mysql://129.194.71.126/dataset")
                 .option("driver", "com.mysql.jdbc.Driver")
@@ -19,8 +20,10 @@ public class Syndesi {
                 .option("user", "spark")
                 .option("password", "spark").load();*/
 
+        // Create the classifier
         NaiveBayesClassifier nb = new NaiveBayesClassifier();
 
+        // Data schema
         StructType schema = new StructType(new StructField[]{
                 new StructField("ap1", DataTypes.IntegerType, false, Metadata.empty()),
                 new StructField("ap2", DataTypes.IntegerType, false, Metadata.empty()),
@@ -30,6 +33,7 @@ public class Syndesi {
                 new StructField("label", DataTypes.DoubleType, false, Metadata.empty()),
         });
 
+        // Train data
         Dataset<Row> trainData = spark.read()
                 .schema(schema)
                 .option("header", "false")
@@ -37,7 +41,7 @@ public class Syndesi {
                 .csv("data/rssData.txt");
 
 
-
+        // Simulate DB connection for test data
         Dataset<Row> testData = spark.read()
                 .schema(schema)
                 .option("header", "false")
@@ -45,8 +49,10 @@ public class Syndesi {
                 .csv("data/testData.txt");
 
 
+        // Train the classifier
         nb.train(trainData);
 
+        // Classify the new RSSI vectors
         nb.classify(testData);
 
         spark.stop();
